e7d7f0d6f682defb06132b30bc6773d171e1a6d2
Ishika <22052461@kiit.ac.in>
2026-01-04 22:32:13 +0530

    Added: Home Page

--- src/app/features/auth/pages/home/home.component.ts
+++ src/app/features/auth/pages/home/home.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule, CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  selectedCategory = 'hotels';
+  searchForm = {
+    location: '',
+    checkIn: '',
+    checkOut: ''
+  };
 
+  constructor(private router: Router) {}
+
+  onSearch() {
+    console.log('Search:', this.searchForm);
+    // TODO: Implement search functionality
+  }
 }
