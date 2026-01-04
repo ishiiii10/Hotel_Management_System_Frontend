d3958c0a6f61c28813f6c088dac9b460558d9171
Ishika <22052461@kiit.ac.in>
2026-01-04 23:11:02 +0530

    Added:Home page Fixed:Login page and Register page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { AuthService } from './features/auth/services/auth.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, CommonModule],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'hms-frontend';
-
-  constructor(public authService: AuthService) {}
-
-  isAuthenticated(): boolean {
-    return this.authService.isAuthenticated();
-  }
-
-  getCurrentUser() {
-    return this.authService.getCurrentUser();
-  }
-
-  logout(): void {
-    this.authService.logout();
-  }
 }
